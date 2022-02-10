800370e81b1d3528ab2697a668af603b0571fc1b
Vadim Lunev <lunev82@gmail.com>
2022-02-10 18:23:09 +0300

    61. cookie, login, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import s from "./Header.module.css";
 import LangContext from "../../LangContext";
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <LangContext.Consumer>
       {(lang) => {
@@ -12,6 +13,13 @@ const Header = () => {
               alt="logo"
               src="https://www.vippng.com/png/full/410-4100150_livechat-logo-png-live-chat.png"
             />
+            <div className={s.loginBlock}>
+              {props.isAuth ? (
+                props.login
+              ) : (
+                <NavLink to={"/login"}>Login</NavLink>
+              )}
+            </div>
             <p>{lang}</p>
           </header>
         );
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -19,7 +19,7 @@ class ProfileContainer extends React.Component {
   }
 
   render() {
-    return <Profile {...this.props} profile={this.props.profile} />;
+    return <Profile profile={this.props.profile} />;
   }
 }
 
