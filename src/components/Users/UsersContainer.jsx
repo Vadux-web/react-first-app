0358d246374d39c5f791dba4d0cee6a146e4cc3e
Vadim Lunev <lunev82@gmail.com>
2022-02-17 09:58:40 +0300

    Pagination bugfix

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -17,13 +17,6 @@ class UsersContainer extends React.Component {
 
   onPageChanged = (pageNumber) => {
     this.props.getUsers(pageNumber, this.props.pageSize);
-
-    // this.props.toggleIsFetching(true);
-    // this.props.setCurrentPage(pageNumber);
-    // usersAPI.getUsers(pageNumber, this.props.pageSize).then((data) => {
-    //   this.props.toggleIsFetching(false);
-    //   this.props.setUsers(data.items);
-    // });
   };
 
   render() {
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -100,6 +100,7 @@ export const getUsers = (currentPage, pageSize) => {
       dispatch(toggleIsFetching(false));
       dispatch(setUsers(data.items));
       dispatch(setTotalUsersCount(data.totalCount));
+      dispatch(setCurrentPage(currentPage));
     });
   };
 };
