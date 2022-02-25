ba5c5ec1694f5d5b0f87c038af704535bb52fa1f
Vadim Lunev <lunev82@gmail.com>
2022-02-25 11:05:42 +0300

    77. redux-form field validation

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 import { Field, reduxForm } from "redux-form";
+import {
+  maxLengthCreator,
+  required,
+} from "../../../utils/validators/validators";
+import { Textarea } from "../../common/FormsControls/FormsControls";
 
 const MyPosts = (props) => {
   const postsElements = props.posts.map((p) => {
@@ -36,11 +41,18 @@ const MyPosts = (props) => {
   );
 };
 
+const maxLength10 = maxLengthCreator(10);
+
 const AddNewPostForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
-        <Field name="newPostText" component="textarea" />
+        <Field
+          name="newPostText"
+          component={Textarea}
+          validate={[required, maxLength10]}
+          placeholder={"Post message"}
+        />
       </div>
       <div>
         <button>Add post</button>
