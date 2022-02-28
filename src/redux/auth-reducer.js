69459070df00f4a021a108b9d9e3cc2196e33501
Vadim Lunev <lunev82@gmail.com>
2022-02-28 10:53:52 +0300

    79. stopSubmit (redux-form)

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,5 +1,6 @@
 import { actions } from "./constans";
 import { authAPI } from "../api/api";
+import { stopSubmit } from "redux-form";
 
 let initialState = {
   userId: null,
@@ -39,6 +40,12 @@ export const login = (email, password, rememberMe) => (dispatch) => {
   authAPI.login(email, password, rememberMe).then((response) => {
     if (response.data.resultCode === 0) {
       dispatch(getAuthUserData());
+    } else {
+      let message =
+        response.data.messages.length > 0
+          ? response.data.messages[0]
+          : "Some error";
+      dispatch(stopSubmit("login", { _error: message }));
     }
   });
 };
